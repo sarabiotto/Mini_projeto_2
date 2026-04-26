63bc61af3b2d93fd3392d1378389f6a225b4aee3
sarabiotto <sarabiotto@gmail.com>
2026-04-26 19:38:45 -0300

    Update lgpd.py

--- lgpd.py
+++ lgpd.py
@@ -3,3 +3,18 @@ from sqlalchemy import String, Date, DateTime, text
 from datetime import datetime
 
 engine = create_engine("postgresql+psycopg2://alunos:AlunoFatec@200.19.224.150:5432/atividade2")
+
+metadata = MetaData()
+
+usuarios = Table(
+    'usuarios', metadata,
+    Column('id', Integer, primary_key=True),
+    Column('nome', String(50), nullable=False, index=True),
+    Column('cpf', String(14), nullable=False),
+    Column('email', String(100), nullable=False, unique=True),
+    Column('telefone', String(20), nullable=False),
+    Column('data_nascimento', Date, nullable=False),
+    Column('created_on', DateTime(), default=datetime.now),
+    Column('updated_on', DateTime(), default=datetime.now, onupdate=datetime.now)
+)
+
